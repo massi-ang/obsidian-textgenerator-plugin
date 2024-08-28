08ffdc7dd70bdc2f86b36c05b1beff2ff658730e
Massimiliano Angelino <angmas@amazon.com>
2024-08-28 11:38:53 +0200

    chore: added model types

--- src/types.ts
+++ src/types.ts
@@ -206,11 +206,11 @@ export type AsyncReturnType<T extends (...args: any) => Promise<any>> =
 
 type LiteralUnion<T extends U, U = string> = T | (U & { zz_IGNORE_ME?: never });
 
-export type Role = LiteralUnion<"assistant" | "user" | "human" | "system" | "admin">;
+export type Role = LiteralUnion<
+  "assistant" | "user" | "human" | "system" | "admin"
+>;
 
 export type Message = {
-  type?: string;
-  image_url?: string;
   role: Role;
   content: MessageContent;
 };
